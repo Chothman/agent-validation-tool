52f9ac487979f6fea52267e3d7680abd54286436
Chothman <cherif.mohamed.cm@gmail.com>
2026-07-01 12:22:01 +0400

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // Paste your deployed GAS Web App /exec URL here after Step: Deploy > New deployment
-const GAS_WEB_APP_URL = 'PASTE_YOUR_DEPLOYED_WEB_APP_URL_HERE';
+const GAS_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbxdT3V1_L5k1kkfR3T8CPLAms3PmjFyXyAhHBHIYrVA6AkGFx8k70S7Ak6MYUSvyjS-wg/exec';
